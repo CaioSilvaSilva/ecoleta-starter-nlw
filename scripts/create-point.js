53a21712e6ccd8adb553ca71b9b2b6b8e005ebb5
Caio Poli <caio.poli@ernetwork.com.br>
2020-07-02 14:27:51 -0300

    :sparkles: Checks for selected items

--- scripts/create-point.js
+++ scripts/create-point.js
@@ -48,10 +48,33 @@ for (const item of itemsToCollect) {
   item.addEventListener("click", handleSelectedItem)
 }
 
+const collectedItems= document.querySelector("input[name=items]")
+
+let selectedItems = []
+
 function handleSelectedItem(event) {
   const itemLi = event.target
 
   itemLi.classList.toggle("selected")
 
   const itemId = event.target.dataset.id
+
+  const alreadySelected = selectedItems.findIndex( item => {
+    const itemFound = item == itemId
+    return itemFound
+  })
+
+  if(alreadySelected >= 0) {
+    const filteredItems = selectedItems.filter(item => {
+      const itemIsDifferent = item != itemId
+      return itemIsDifferent
+    })
+
+    selectedItems = filteredItems
+  } else {
+    selectedItems.push(itemId)
+  }
+
+  collectedItems.value = selectedItems
+
 }
